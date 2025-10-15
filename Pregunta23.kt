8fdbaf1b8c34f8e1a46ffad195998c8e881f8b4a
FerSV4 <124794533+FerSV4@users.noreply.github.com>
2025-10-15 13:42:42 -0400

    Enunciado pregunta 23

---
+++ Pregunta23.kt
@@ -0,0 +1,15 @@
+/* 
+23. Ordenamiento (10 puntos):
+Implementa el método más básico de Ordenamiento de una lista de números,
+bubbleSort, y también la llamada al método:
+fun bubbleSort(input: MutableList<Int>): MutableList<Int> {
+// TODO: Implementa el método
+}
+fun main() {
+val numeros: MutableList<Int> = mutableListOf(64, 34, 25,
+12, 22, 11, 90)
+println("Lista original: $numeros")
+// TODO: Escribe la llamada al método
+println("Lista ordenada: $numeros")
+}
+*/
