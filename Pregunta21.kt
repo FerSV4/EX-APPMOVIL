aa198e18e74a56a73d7340c5e532e27036c68847
FerSV4 <ferandressc@hotmail.com>
2025-10-15 14:04:50 -0400

    Enunciado pregunta 21

---
+++ Pregunta21.kt
@@ -0,0 +1,17 @@
+/*
+21. Problema: El cuarto del caos(10 puntos):
+Bit es un chico muy travieso y descuidado, tiene su cuarto muy desordenado y hasta
+el momento esto no fué algo que le preocupara. Sin embargo, le han pedido que
+lleve a la escuela su colección de números enteros, pero el problema es que tiene su
+cuarto tan desordenado que tiene números enteros, strings y otros tipos de datos
+que no puede encontrar todos los ejemplares y necesita tu ayuda.
+Instrucciones
+Ayuda a Bit a clasificar y obtener todos los números enteros de su cuarto para que
+pueda llevarlos a su escuela.
+Crea una función con el siguiente formato:
+fun classifyIntegers ( items : List <Any>) : List <Int> {
+
+// Escribe tu codigo aqui
+// retorna tu resultado
+}
+ */
