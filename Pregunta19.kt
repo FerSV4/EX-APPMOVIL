79fb34123e314c3c0466b79bf3b048a89f58f35e
FerSV4 <ferandressc@hotmail.com>
2025-10-15 14:35:19 -0400

    Ejercicio Incorrecto

--- Pregunta19.kt
+++ Pregunta19.kt
@@ -3,3 +3,8 @@
 String y retorne true si es un palíndromo (se lee igual al derecho y al revés) o false si
 no lo es. Ejemplo: "ana" → true, "hola" → false.
  */
+
+esPalindromo(Palabra : String) : Boolean {
+    val palabraformateada = Palabra.lowercase()
+    return PalabraFormateada.Reversed()
+}
