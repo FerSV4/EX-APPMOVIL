56b02078b0d12ea98e9dfda02dd68f079a43d349
FerSV4 <ferandressc@hotmail.com>
2025-10-15 14:29:14 -0400

    Anunciado pregunta 19

---
+++ Pregunta19.kt
@@ -0,0 +1,5 @@
+/*
+19. Manipulación de Strings (5 puntos): Crea una función esPalindromo que reciba un
+String y retorne true si es un palíndromo (se lee igual al derecho y al revés) o false si
+no lo es. Ejemplo: "ana" → true, "hola" → false.
+ */
